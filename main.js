d884f8795ee26fc975a8e7b10d02657459966b37
Theo Arini <tarini@z4r2p2.42lyon.fr>
2025-06-28 13:48:08 +0200

    f

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 let solution = '';
 
-function get_word() {
+function get_word()
+{
   const index = Math.floor(Math.random() * wordList.length);
   return wordList[index];
 }
 
-function is_valid(guess, solution) {
+function is_valid(guess, solution)
+{
   return guess == solution
 }
 
-async function startup() {
+async function startup()
+{
   const game = document.getElementById('game');
   drawGrid(game);
   solution = get_word();
